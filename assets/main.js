f449e6145565b0b733d502a9ccf980c7692ff75f
Rendi <rendicrew96@gmail.com>
2022-10-31 23:00:02 +0700

    update

--- assets/main.js
+++ assets/main.js
@@ -2,3 +2,25 @@
 function mySubmit(e){
     alert("The form was submitted")
 }
+window.addEventListener("scroll",function(){
+    let reveal = document.querySelectorAll(".reveal")
+    let reveal2= document.querySelectorAll(".reveal2")
+
+    for(i=0;i<reveal.length;i++){
+        let revealTop = reveal[i].getBoundingClientRect().top
+        if(revealTop < 350){
+            reveal[i].classList.add("nyala")
+        }else reveal[i].classList.remove("nyala")
+    }
+
+    for(i=0;i<reveal2.length;i++){
+
+        let revealTop2 = reveal2[i].getBoundingClientRect().top
+        console.log(revealTop2);
+
+        if(revealTop2 < 350){
+            reveal2[i].classList.add("nyala")
+        }else reveal2[i].classList.remove("nyala")
+    }
+
+})
