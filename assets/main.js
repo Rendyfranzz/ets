62973917f4ec62837a82bd23f3a363ccddddae69
Rendi <rendicrew96@gmail.com>
2022-10-31 19:22:22 +0700

    update

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,4 @@
-const local = document.getElementsByName("lokasi")
-function addLocation (){
-    for (var i = 0, length = local.length; i < length; i++) {
-        if (local[i].checked) {
-          console.log(local[i].value);
-          break;
-        }
-      }
-}
 
-document.addEventListener("DOMContentLoaded",function(){
-    addLocation()
-})
+function mySubmit(e){
+    alert("The form was submitted")
+}
